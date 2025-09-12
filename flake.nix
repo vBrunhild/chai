d9d0d714d7f988aca26aa672781cabe62fe3cfce
vBrunhild <brunomoretti100@gmail.com>
2025-09-12 09:09:29 -0400

    chore(flake): create dev flake nix file

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Chai devflake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    wild = {
+      url = "github:davidlattimore/wild";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    wild,
+    ...
+  }: let
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+
+    pkgsFor = system:
+      import nixpkgs {
+        inherit system;
+        overlays = [(import wild)];
+      };
+
+    allPkgs = forAllSystems pkgsFor;
+  in {
+    devShells = forAllSystems (
+      system: let
+        pkgs = allPkgs.${system};
+        wildStdenv = pkgs.useWildLinker pkgs.stdenv;
+      in {
+        default = pkgs.mkShell.override {stdenv = wildStdenv;} {
+          packages = [
+            pkgs.cargo
+            pkgs.clippy
+            pkgs.rust-analyzer
+            pkgs.rustc
+            pkgs.rustfmt
+            pkgs.sqlx-cli
+          ];
+        };
+      }
+    );
+  };
+}
